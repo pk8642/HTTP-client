a2c905cbefb12ab0bda58fbdd027d9d93da1e6bb
Pavel Kalugin <pkalugin48@gmail.com>
2019-10-03 00:48:52 +0500

    rewrite for flexibility (I believe)

--- test_client.py
+++ test_client.py
@@ -8,32 +8,14 @@ import io
 
 
 class TestArguments(TestCase):
-    # @staticmethod
-    # def parse_args(line):
-    #     args_array = line.split()
-    #     result = []
-    #     temp = ''
-    #     for e in args_array:
-    #         if len(temp) > 0:
-    #             temp += f' {e}'
-    #             if '"' in e:
-    #                 result.append(temp[:-1])
-    #                 temp = ''
-    #         else:
-    #             if '"' in e:
-    #                 temp = e[1:]
-    #             else:
-    #                 result.append(e)
-    #     return result
-
     def check_args(self, args, host=None):
         if host:
             _host, _ = HTTP_client.parse_uri(args[0])
             self.assertEqual(_host, host)
         testing_req = Request(None, None, *args[1:])
-        self.assertEqual(testing_req._method, args[1])
-        self.assertEqual(testing_req._headers, args[2])
-        self.assertEqual(testing_req._body, args[3])
+        self.assertEqual(testing_req.method, args[1])
+        self.assertEqual(testing_req.headers, args[2])
+        self.assertEqual(testing_req.body, args[3])
 
     def check_parsing_input(self, line_input, host=None):
         parser = parse_line.create_parser()
@@ -83,17 +65,77 @@ class TestArguments(TestCase):
         self.check_parsing_input(line.split())
 
 
-def get_fake_socket(text, to=15):
+def get_fake_socket(text=b'', to=15):
     sock = mock.Mock()
-    sock.makefile.return_value = get_fake_reader(text)
-    sock.sendall.return_value = len(text)
+    sock.makefile.return_value = io.BytesIO(text)
+    sock.sendall.return_value = None
     sock.settimeout.return_value = to
     sock.connect.return_value = None
+    sock.text = text
     return sock
 
 
-def get_fake_reader(text):
+def get_fake_reader(text=b''):
     return io.BytesIO(text)
+
+
+class MockResponse:
+    def __init__(self):
+        self.sock = None
+        self.response_headers = None
+        self.response_body = None
+        self.headers = None
+        self.cookies = None
+        self.filename = None
+        self.ext = None
+        self.charset = None
+        self.connection = True
+
+        self.response = Response(get_fake_socket(b''))
+
+    def _set_text_to_sock(self, text, to=15):
+        self.response.sock = get_fake_socket(text, to)
+
+    def _get_reader(self, text):
+        self._set_text_to_sock(text)
+        return self.response.sock.makefile()
+
+    def _set_body(self, text):
+        self.response.response_body = text
+
+    def receive_headers(self, text):
+        reader = self._get_reader(text)
+        self.response.receive_headers(reader)
+
+        self.headers = self.response.headers
+        self.response_headers = self.response.response_headers
+        self.cookies = self.response.cookies
+
+    def get_filename(self, path):
+        self.response.get_filename(path)
+
+        self.filename, self.ext = self.response.filename, self.response.ext
+
+    def static_recv(self, text, length):
+        reader = self._get_reader(text)
+        self.response.static_recv(reader, length)
+
+        self.response_body = self.response.response_body
+
+    def dynamic_recv(self, text):
+        reader = self._get_reader(text)
+        self.response.dynamic_recv(reader)
+
+        self.response_body = self.response.response_body
+
+    def receive(self):
+        self.response.receive()
+
+        self.headers = self.response.headers
+        self.response_headers = self.response.response_headers
+        self.cookies = self.response.cookies
+        self.response_body = self.response.response_body
+        self.charset = self.response.charset
 
 
 class TestFunctionalityResponse(TestCase):
@@ -115,73 +157,75 @@ class TestFunctionalityResponse(TestCase):
 
     def test_receive_headers(self):
         text = b'HTTP-code\r\nHeader: value\r\n\r\n'
-        self.response.receive_headers(get_fake_reader(text))
+        response = MockResponse()
+        response.receive_headers(text)
 
-        self.assertEqual(self.response.headers,
+        self.assertEqual(response.headers,
                          {
                              'code': 'HTTP-code\r\n',
                              'header': 'value\r\n'
                          })
-        self.assertEqual(self.response.response_headers,
+        self.assertEqual(response.response_headers,
                          'Header: value\r\n')
-        self.clear_response()
 
     def test_getting_cookie(self):
         text = b'code\r\nSet-Cookie: cookie\r\n\r\n'
-        self.response.receive_headers(get_fake_reader(text))
+        response = MockResponse()
+        response.receive_headers(text)
 
-        self.assertEqual(self.response.cookies, ['cookie\r\n'])
-        self.clear_response()
+        self.assertEqual(response.cookies, ['cookie\r\n'])
 
     def test_getting_multiple_cookies(self):
         text = b'code\r\nSet-Cookie: cookie1\r\nSet-Cookie: cookie2\r\n\r\n'
-        self.response.receive_headers(get_fake_reader(text))
+        response = MockResponse()
+        response.receive_headers(text)
 
-        self.assertEqual(self.response.cookies, ['cookie1\r\n', 'cookie2\r\n'])
-        self.clear_response()
+        self.assertEqual(response.cookies, ['cookie1\r\n', 'cookie2\r\n'])
 
     def test_getting_filename_from_filepath(self):
         path = 'www.example.com/some-file.some-ext'
-        self.response.get_filename(path)
+        response = MockResponse()
+        response.get_filename(path)
 
-        self.assertEqual((self.response.filename, self.response.ext),
+        self.assertEqual((response.filename, response.ext),
                          ('some-file', 'some-ext'))
-        self.clear_response()
 
     def test_getting_filename_from_html(self):
         path = 'www.example.com/some-page'
-        self.response.response_body = b'tatata<title>Title of the page</title>'
-        self.response.charset = 'utf8'
-        self.response.get_filename(path)
+        html = b'tatata<title>Title of the page</title>'
+        response = MockResponse()
+        response._set_body(html)
+        response.response.charset = 'utf8'
+        response.get_filename(path)
 
-        self.assertEqual((self.response.filename, self.response.ext),
+        self.assertEqual((response.filename, response.ext),
                          ('Title of the page', 'html'))
-        self.clear_response()
 
     def test_not_getting_filename(self):
         path = 'www.bad-path.com/'
-        self.response.response_body = b'very bad body'
-        self.response.charset = 'utf8'
-        self.response.get_filename(path)
+        html = b'very bad body'
+        response = MockResponse()
+        response._set_body(html)
+        response.response.charset = 'utf8'
+        response.get_filename(path)
 
-        self.assertEqual((self.response.filename, self.response.ext),
+        self.assertEqual((response.filename, response.ext),
                          ('received', 'html'))
-        self.clear_response()
 
     def test_static_recv(self):
         text = b'abrakadabra'
-        reader = get_fake_reader(text)
-        self.response.static_recv(reader, len(text))
+        response = MockResponse()
+        response.static_recv(text, len(text))
 
-        self.assertEqual(self.response.response_body, text)
+        self.assertEqual(response.response_body, text)
         self.clear_response()
 
     def test_dynamic_recv(self):
         text = b'8\r\n12345678\r\n0\r\n\r\n'
-        reader = get_fake_reader(text)
-        self.response.dynamic_recv(reader)
+        response = MockResponse()
+        response.dynamic_recv(text)
 
-        self.assertEqual(self.response.response_body, b'12345678')
+        self.assertEqual(response.response_body, b'12345678')
         self.clear_response()
 
     def test_full_receive_with_handled_headers(self):
@@ -207,106 +251,135 @@ class TestFunctionalityResponse(TestCase):
         self.assertEqual(len(self.response.response_body), 14)
 
 
+class MockRequest:
+    def __init__(self):
+        self.host = None
+        self.path = None
+        self.method = None
+        self.headers = None
+        self.body = None
+        self.timeout = None
+        self.request = None
+
+    def _create_request(self, host='www.example.com', path='/', method='GET',
+                        headers=None, body='', to=15):
+        if headers is None:
+            headers = []
+
+        self.request = Request(host, path, method, headers, body, to)
+
+    def form_message(self):
+        result = self.request.form_message()
+
+        self.headers = self.request.headers
+        return result
+
+    def modify_data(self):
+        self.request.modify_data()
+
+        self.body = self.request.body
+        self.headers = self.request.headers
+
+    def send_data(self, sockets):
+        with mock.patch('socket.socket', get_fake_socket):
+            return self.request.send_data(sockets)
+
+    def set_cookies(self, cookies):
+        self.request.set_cookies(cookies)
+
+        self.headers = self.request.headers
+
+
 class TestFunctionalityRequest(TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.request = None
 
-        self.args = [
-            'www.example.com',  # host 0
-            '/',  # path 1
-            'GET',  # method 2
-            [],  # headers 3
-            ''  # body 4
-        ]
-
-    def get_args(self):
-        return self.args.copy()
-
-    def prepare_request(self, args):
-        if self.request is None:
-            self.request = Request(*args)
-        else:
-            attributes = self.request.__dict__
-            iterator = args.__iter__()
-            for attribute in attributes[:-1]:
-                attributes[attribute] = iterator.__next__()
+    # def get_args(self):
+    #     return self.args.copy()
+    #
+    # def prepare_request(self, args):
+    #     if self.request is None:
+    #         self.request = Request(*args)
+    #     else:
+    #         attributes = self.request.__dict__
+    #         iterator = args.__iter__()
+    #         for attribute in attributes[:-1]:
+    #             attributes[attribute] = iterator.__next__()
 
     def test_forming_messages_with_headers(self):
-        args = self.get_args()
-        args[3] = ['Header: value', 'Another-Header: value']
-        args[4] = 'somebody'
-        self.prepare_request(args)
+        request = MockRequest()
+        request._create_request(headers=['Header: value',
+                                         'Another-Header: value'],
+                                body='somebody')
 
         expect = b'GET / HTTP/1.1\r\nHost: www.example.com\r\nHeader: value' \
                  b'\r\nAnother-Header: value\r\n'
-        actual = self.request._form_message('/', 'www.example.com')
+        actual = request.form_message()
 
         self.assertEqual(expect, actual)
 
     def test_modifying_data_with_body(self):
-        args = self.get_args()
-        args[3] = ['Access-Encoding: gzip', 'Content-Type: plain/text; '
+        headers = ['Access-Encoding: gzip', 'Content-Type: plain/text; '
                                             'charset=utf8']
-        args[4] = 'look at me, I\'m body'
-        self.prepare_request(args)
+        body = 'look at me, I\'m body'
 
-        self.request._modify_data()
+        request = MockRequest()
+        request._create_request(headers=headers,
+                                body=body)
 
-        self.assertEqual(self.request._headers, [
+        request.modify_data()
+
+        self.assertEqual(request.headers, [
             'Access-Encoding: gzip',
             'Content-Type: plain/text; charset=utf8',
-            f'Content-Length: {len(args[4])}\r\n'
+            f'Content-Length: {len(body)}\r\n'
         ])
 
     def test_modifying_data_with_no_body(self):
-        args = self.args
-        args[3] = ['Access-Encoding: gzip', 'Content-Type: plain/text; '
+        headers = ['Access-Encoding: gzip', 'Content-Type: plain/text; '
                                             'charset=utf8']
-        self.prepare_request(args)
+        request = MockRequest()
+        request._create_request(headers=headers)
 
-        self.request._modify_data()
+        request.modify_data()
 
-        self.assertEqual(self.request._headers, [
+        self.assertEqual(request.headers, [
             'Access-Encoding: gzip',
             'Content-Type: plain/text; charset=utf8\r\n',
         ])
 
     def test_sending_data_with_existing_socket(self):
-        args = self.args
-        sockets = {args[0]: get_fake_socket(b'')}
-        self.prepare_request(args)
+        request = MockRequest()
+        request._create_request()
+        sockets = {request.request.host: get_fake_socket(b'')}
 
-        self.request.send_data(sockets)
+        request.send_data(sockets)
 
         expected = b'GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n'
-
-        sockets[args[0]].sendall.assert_called_with(expected)
+        sockets[request.request.host].sendall.assert_called_with(expected)
 
     def test_sending_data_with_no_existing_socket(self):
-        args = self.args
-        self.prepare_request(args)
-        mock_sock = get_fake_socket(b'')
+        request = MockRequest()
+        request._create_request()
 
-        with mock.patch('socket.socket', mock_sock):
-            mock_sock = self.request.send_data({})
+        mock_sock = request.send_data({})
 
         expected = b'GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n'
-
         mock_sock.sendall.assert_called_with(expected)
 
     def test_setting_cookies(self):
-        args = self.args
-        self.prepare_request(args)
+        request = MockRequest()
+        request._create_request()
 
         cookies = {
             'www.example.com': ['some-cookie=value; path=/; '
                                 'expires=Mon, 28-Oct-2019 14:24:42 GMT\r\n']
         }
 
-        self.request.set_cookies(cookies)
+        request.set_cookies(cookies)
 
-        self.assertEqual(self.request._headers, ['Cookie: some-cookie=value'])
+        self.assertEqual(request.headers, ['Cookie: some-cookie=value'])
 
 
 if __name__ == '__main__':
